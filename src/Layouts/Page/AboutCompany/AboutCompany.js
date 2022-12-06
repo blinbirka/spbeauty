d2426df16a1fe32b23273cf07c66ade3eb37bd72
blinbirka <109791621+blinbirka@users.noreply.github.com>
2022-12-06 14:15:38 +0000

    блок

--- src/Layouts/Page/AboutCompany/AboutCompany.js
+++ src/Layouts/Page/AboutCompany/AboutCompany.js
@@ -3,11 +3,11 @@ import General from "./Components/General/General";
 import Addresses from "./Components/Addresses/Addresses";
 function AboutCompany() {
     return (
-        <div>
+        <div className="row">
             <Heading/> {/* Заголовок и кнопка справа */}
-            <div className='d-flex flex-column mt-5'>
-                <General/>  {/* Основная информация вроде названия, типа и телефона */}
-                <Addresses/> {/* Список доступных адресов заведений */}
+            <div className='d-flex mt-5'>
+                <General className='col'/>  {/* Основная информация вроде названия, типа и телефона */}
+                <Addresses className='col'/> {/* Список доступных адресов заведений */}
             </div>
         </div>
     );
--- src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
@@ -4,13 +4,17 @@ import ListElement from "./ListElement";
 
 function Addresses() {
     return (
-        <div className='d-inline-flex flex-column gap-2 mt-4'>
-            <div className='title'>Адреса заведений</div>
-            <div className='d-inline-flex flex-column gap-3'>
-                <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
-                <ListElement>Санкт-Петербург, Казанская ул, 7</ListElement>
-                <SecondaryButton>Добавить филиал</SecondaryButton>
+        <div className='d-inline-flex flex-column justify-content-between mt-4 panel1'>
+            <div className='d-inline-flex flex-column'>
+                <div class="d-flex flex-column gap-4">
+                    <div className='title'>Адреса заведений</div>
+                    <div class="d-flex flex-column gap-2">               
+                        <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
+                        <ListElement>Санкт-Петербург, Казанская ул, 7</ListElement>
+                    </div>
+                </div>
             </div>
+            <SecondaryButton>Добавить филиал</SecondaryButton>
         </div>
     );
 }
