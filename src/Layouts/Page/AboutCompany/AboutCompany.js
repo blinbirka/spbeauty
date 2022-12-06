a27730d83385aaf670152760abf9f5249e6a23ed
blinbirka <109791621+blinbirka@users.noreply.github.com>
2022-12-06 15:48:07 +0000

    -

--- src/Layouts/Page/AboutCompany/AboutCompany.js
+++ src/Layouts/Page/AboutCompany/AboutCompany.js
@@ -3,11 +3,11 @@ import General from "./Components/General/General";
 import Addresses from "./Components/Addresses/Addresses";
 function AboutCompany() {
     return (
-        <div className="row">
+        <div class="row" className="d-flex-item">
             <Heading/> {/* Заголовок и кнопка справа */}
             <div className='d-flex mt-5'>
-                <General className='col'/>  {/* Основная информация вроде названия, типа и телефона */}
-                <Addresses className='col'/> {/* Список доступных адресов заведений */}
+                <General class='col'/>  {/* Основная информация вроде названия, типа и телефона */}
+                <Addresses class='col'/> {/* Список доступных адресов заведений */}
             </div>
         </div>
     );
